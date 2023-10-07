4a4251bfab6cf5f2c2ed1f496ed0b8d49b4177ae
Usgupta <umanggupta1103@gmail.com>
2023-10-07 23:16:03 +0800

    scene 2 basic

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using TMPro;
-using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
